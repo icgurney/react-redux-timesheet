e0e5aeafab0f1b98facde36ed85bd0c712365db5
Ian Gurney <ianclaytongurney@gmail.com>
2019-05-11 12:37:06 -0500

    We added some routes

--- lab-03/src/App.js
+++ lab-03/src/App.js
@@ -3,17 +3,28 @@ import './App.css';
 import Projects from "./projects/Projects";
 import Employees from './employees/Employees';
 import Timesheets from './timesheets/Timesheets';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Navigation from './nav/Navigation';
 
 class App extends React.Component {
-  render() {
-    return (
+
+render() {
+  return (
+    <BrowserRouter>
       <div className="App">
+      <Navigation />
         <div className="container">
-          <Projects />
+          <Switch>
+            <Route path="/projects" component={Projects}/>
+            <Route path="/employees" component={Employees}/>
+            <Route path="/timesheets" component={Timesheets}/>
+            <Redirect to="/employees"/>
+          </Switch>
         </div>
       </div>
-    );
-  }
+    </BrowserRouter>
+  );
+}
 }
 
 export default App;
